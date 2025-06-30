b09d3ab7588d29073def2016bfd74661f0a719ba
anand-106 <anands2003106@gmail.com>
2025-06-30 22:58:37 +0530

    checkbox colr change

--- frontend/src/components/ReadmePage/preferencesComp.js
+++ frontend/src/components/ReadmePage/preferencesComp.js
@@ -191,7 +191,7 @@ function CheckListItem({ text, checked, onChange }) {
         type="checkbox"
         checked={checked}
         onChange={onChange}
-        className="w-5 h-5 accent-white appearance-auto"
+        className="w-5 h-5 accent-[#030617] rounded  appearance-auto"
       />
       <span className="text-white font-figtree text-base font-medium">
         {text}
